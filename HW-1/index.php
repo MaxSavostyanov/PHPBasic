399cb0d16349f8390ab816678e5c43f60d58d0cb
Max Savostyanov <maxsavostyanov34@gmail.com>
2019-02-01 23:31:26 +0300

    Update index.php

--- HW-1/index.php
+++ HW-1/index.php
@@ -7,16 +7,17 @@ $year =  date('Y');
 <!DOCTYPE html>
 <html>
 <head>
-  <title><?=$title?></title>
+  <title><?=$title;?></title>
   <meta charset="UTF-8">
 </head>
 <body>
-<h1><?=$h1?></h1>
+Вариант 1
+<h1><?=$h1;?></h1>
 Краткая биография обо мне<br>
 Родился в 1990 году в городе Волгоград. Закончил в 2013 году НИУ МЭИ.<br>
 На данный момент учусь в <b>GeekUniversity</b> на факультете Веб-разработки.
 <br><br>
 
-<?=$year?>
+<?=$year;?>
 </body>
 </html>
