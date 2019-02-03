72033ccdae4bf6d31219ae6b3939def001c855cd
Max Savostyanov <maxsavostyanov34@gmail.com>
2019-02-03 20:15:46 +0300

    Create Task_1.php
    Объявить две целочисленные переменные $a и $b и задать им произвольные начальные значения. Затем написать скрипт, который работает по следующему принципу:
    если $a и $b положительные, вывести их разность;
    если $а и $b отрицательные, вывести их произведение;
    если $а и $b разных знаков, вывести их сумму;
    Ноль можно считать положительным числом.

---
+++ HW-2/Task_1.php
@@ -0,0 +1,27 @@
+<?php
+
+function script($a, $b)
+{
+  if ($a >= 0 && $b >= 0) {
+    $res = $a - $b;
+    return $res;
+  } elseif ($a < 0 && $b < 0) {
+    $res = $a * $b;
+    return $res;
+  } else {
+    $res = $a + $b;
+    return $res;
+  }
+}
+
+$a = 5;
+$b = 3;
+echo script($a, $b) . "<br>";
+
+$a = -5;
+$b = -3;
+echo script($a, $b) . "<br>";
+
+$a = 5;
+$b = -3;
+echo script($a, $b) . "<br>";
