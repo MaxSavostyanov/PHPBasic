7932622914daa4e38f61c86a81f01a0425b141a9
Max Savostyanov <maxsavostyanov34@gmail.com>
2019-02-03 22:10:38 +0300

    Create Task_6.php
    *С помощью рекурсии организовать функцию возведения числа в степень. Формат: function power($val, $pow), где $val – заданное число, $pow – степень. Отрицательные можно не делать. При желании сделайте фукнцию анонимной и организуйте рекурсию через замыкание.

---
+++ HW-2/Task_6.php
@@ -0,0 +1,14 @@
+<?php
+function power($val, $pow)
+{
+  if ($pow < 0) {
+    return "!!возведение только в положительную степень";
+  } elseif ($pow > 0) {
+    return $val * power($val, $pow - 1);
+  } else {
+    return 1;
+  }
+}
+
+
+echo "2 ^ 10 = " . power(2, 10);
