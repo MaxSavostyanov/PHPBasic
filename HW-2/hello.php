24a17e5ad7952b41a05b08824bd3167c6ec5a462
Max Savostyanov <maxsavostyanov34@gmail.com>
2019-02-03 21:28:41 +0300

    Create hello.php for Task_5
    шаблон страницы с приветсвием.

---
+++ HW-2/hello.php
@@ -0,0 +1,4 @@
+<h1>Hello!</h1>
+<div style="width: 300px">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aut delectus ea
+  eligendi eosesse estillum incidunt mollitia nisi, omnis quibusdam rem reprehenderit tenetur, ullam vel velit
+  voluptatibus! Saepe.</div>
