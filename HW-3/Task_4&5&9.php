43e9a9b0b546b0a0d8774eb49d3190b869f33355
Max Savostyanov <maxsavostyanov34@gmail.com>
2019-02-09 23:10:35 +0300

    Update Task_4&5&9.php
    - убрал заглавные буквы из массива;
    - добавил в функцию обратоку регистра буквы;
    - заменил str_split на preg_split;
    -  убрал объединенную функцию. прописал вызов функций друг за другом.

--- HW-3/Task_4&5&9.php
+++ HW-3/Task_4&5&9.php
@@ -1,60 +1,43 @@
 <?php
 //Задание #4
 $alphabetRu = [
-  "a" => "a", "А" => "A",
-  "б" => "b", "Б" => "B",
-  "в" => "v", "В" => "V",
-  "г" => "g", "Г" => "G",
-  "д" => "d", "Д" => "D",
-  "е" => "e", "Е" => "E",
-  "ё" => "yo", "Ё" => "Yo",
-  "ж" => "g", "Ж" => "G",
-  "з" => "z", "З" => "Z",
-  "и" => "i", "И" => "I",
-  "й" => "y", "Й" => "Y",
-  "к" => "k", "К" => "K",
-  "л" => "l", "Л" => "L",
-  "м" => "m", "М" => "M",
-  "н" => "n", "Н" => "N",
-  "о" => "o", "О" => "O",
-  "п" => "p", "П" => "P",
-  "р" => "r", "Р" => "R",
-  "с" => "s", "С" => "S",
-  "т" => "t", "Т" => "T",
-  "у" => "u", "У" => "U",
-  "ф" => "f", "Ф" => "F",
-  "х" => "kh", "Х" => "Kh",
-  "ц" => "c", "Ц" => "C",
-  "ч" => "ch", "Ч" => "Ch",
-  "ш" => "sh", "Ш" => "Sh",
-  "щ" => "shh", "Щ" => "Shh",
-  "ъ" => "\'", "Ъ" => "\'",
-  "ы" => "u", "Ы" => "U",
-  "ь" => "\'", "Ь" => "\'",
-  "э" => "eh", "Э" => "Eh",
-  "ю" => "yu", "Ю" => "?u",
-  "я" => "ya", "Я" => "?a",
-  " " => " "
+  "a" => "a", "б" => "b", "в" => "v", "г" => "g",
+  "д" => "d", "е" => "e", "ё" => "yo", "ж" => "g",
+  "з" => "z", "и" => "i", "й" => "y", "к" => "k",
+  "л" => "l", "м" => "m", "н" => "n", "о" => "o",
+  "п" => "p", "р" => "r", "с" => "s", "т" => "t",
+  "у" => "u", "ф" => "f", "х" => "kh", "ц" => "c",
+  "ч" => "ch", "ш" => "sh", "щ" => "shh", "ъ" => "\'",
+  "ы" => "u", "ь" => "\'", "э" => "eh", "ю" => "yu",
+  "я" => "ya"
 ];
 
-function translate($string, $alphabet)
+function translit($string, $alphabet)
 {
   $newString = '';
   for ($i = 0; $i < mb_strlen($string); $i++) {
     $letter = mb_substr($string, $i, 1);
-    $newString .= isset($alphabet[$letter]) ? $alphabet[$letter] : $letter;
+    if(isset($alphabet[mb_strtolower($letter)])){
+      if (mb_strtolower($letter) == $letter){
+        $newString .= $alphabet[$letter];
+      }else{
+        $newString .= mb_strtoupper($alphabet[mb_strtolower($letter)]);
+      }
+    }else{
+      $newString .= $letter;
+    }
   }
   return $newString;
 }
 
-$string = "Привет, Мир! Меня зовут Максим!";
+$string = "Привет, Нир! Меня зовут Максим!";
 echo "Оригинал: {$string}<br>";
-echo "Транслит: " . translate($string, $alphabetRu) . "<br><br>";
+echo "Транслит: " . translit($string, $alphabetRu) . "<br><br>";
 
 //Задание #5
 function replace($string)
 {
-  $strArr = str_split($string);
+  $strArr = preg_split('//u', $string, NULL, PREG_SPLIT_NO_EMPTY);
   foreach ($strArr as $key => $value) {
     if ($value == " ") {
       $strArr[$key] = "_";
@@ -69,20 +52,6 @@ $str = replace($str);
 echo "{$str}<br><br>";
 
 //Задание #9
-function translitURL($string, $alphabet)
-{
-  $newString = '';
-  for ($i = 0; $i < mb_strlen($string); $i++) {
-    $letter = mb_substr($string, $i, 1);
-    if ($letter == " ") {
-      $newString .= "_";
-    } else {
-      $newString .= isset($alphabet[$letter]) ? $alphabet[$letter] : $letter;
-    }
-  }
-  return $newString;
-}
-
 $string = "Привет, Мир! У нас все хорошо!";
 echo "Оригинал: {$string}<br>";
-echo "Транслит: " . translitURL($string, $alphabetRu);
+echo "Транслит: " . replace(translit($string, $alphabetRu));
