4b7719fdb60486f1fe2d771eb6e8488a90c7b385
Max Savostyanov <maxsavostyanov34@gmail.com>
2019-02-12 22:26:31 +0300

    Update upload.php
    - вместо копирования файла из каталога временного хранения сделал перенос;
    - добавил проверку расширения загружаемого файла.

--- HW-4/upload.php
+++ HW-4/upload.php
@@ -2,23 +2,28 @@
 header('Content-Type: text/html; charset=utf-8');
 // Каталог, в который мы будем принимать файл:
 $uploaddirMax = './img-max/';
-$uploadfileMax = $uploaddirMax.basename($_FILES['uploadfile']['name']);
+$uploadfileMax = $uploaddirMax . basename($_FILES['uploadfile']['name']);
 $uploaddirMin = './img-min/';
-$uploadfileMin = $uploaddirMin.basename($_FILES['uploadfile']['name']);
+$uploadfileMin = $uploaddirMin . basename($_FILES['uploadfile']['name']);
+$typeUploadFile = $_FILES['uploadfile']['type'];
 
-// Копируем файл из каталога для временного хранения файлов:
-if (copy($_FILES['uploadfile']['tmp_name'], $uploadfileMax))
+function isPicture($typeUploadFile)
 {
-  echo "<h3>Файл успешно загружен на сервер</h3>";
+  return in_array($typeUploadFile, ["image/jpeg", "image/png", "image/gif"]);
 }
-else { echo "<h3>Ошибка! Не удалось загрузить файл на сервер!</h3>"; exit; }
 
-// Выводим информацию о загруженном файле:
-echo "<h3>Информация о загруженном на сервер файле: </h3>
-<p><b>Оригинальное имя загруженного файла: ".$_FILES['uploadfile']['name']."</b></p>
-<p><b>Mime-тип загруженного файла: ".$_FILES['uploadfile']['type']."</b></p>
-<p><b>Размер загруженного файла в байтах: ".$_FILES['uploadfile']['size']."</b></p>
-<p><b>Временное имя файла: ".$_FILES['uploadfile']['tmp_name']."</b></p>";
+function isLoaded($uploadfileMax)
+{
+  return move_uploaded_file($_FILES['uploadfile']['tmp_name'], $uploadfileMax);
+}
+
+// Переносим файл из каталога для временного хранения файлов:
+if (isPicture($typeUploadFile) && isLoaded($uploadfileMax)) {
+  echo "<h3>Файл успешно загружен на сервер</h3>";
+} else {
+  echo "<h3>Ошибка! Не удалось загрузить файл на сервер!</h3>";
+  exit;
+}
 
 include('classSimpleImage.php');
 $image = new SimpleImage();
